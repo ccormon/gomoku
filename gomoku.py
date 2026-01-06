91c1d5b9a0164c489b1ff0921f08e7aa82945baf
ccormon <camille.cormon@gmail.com>
2026-01-06 14:38:36 +0100

    complete and clean the test function

--- gomoku.py
+++ gomoku.py
@@ -3,11 +3,43 @@ import utils
 
 class Gomoku():
 	def __init__(self):
-		self.boardMap = [[0 for j in range(utils.BOARDSIZE)] for i in range(utils.BOARDSIZE)]
+		self.boardMap = [[0 for _ in range(utils.BOARDSIZE)] for _ in range(utils.BOARDSIZE)]
 		self.depth = utils.DEPTH
 
-	def get_boardMap(self):
-		return self.boardMap
 
-	def get_depth(self):
-		return self.depth
+	"""
+	TEST FUNCTION TO REMOVE BEFORE SUBMIT
+	"""
+	def drawBoardInTerminal(self):
+		print("        ", end="")
+		for i in range(utils.BOARDSIZE):
+			if i < 10:
+				print(f"{i}   ", end="")
+			else:
+				print(f"{i}  ", end="")
+		print()
+		print()
+
+		for i in range(utils.BOARDSIZE):
+			for j in range(utils.BOARDSIZE):
+				if j == 0:
+					if i < 10:
+						print(f"{i}       ", end="")
+					else:
+						print(f"{i}      ", end="")
+				match self.boardMap[i][j]:
+					case 1:
+						state = utils.PLAYER
+					case 2:
+						state = utils.AI
+					case _:
+						state = utils.EMPTY
+				print(f"{state}   ", end="")
+			print()
+		print()
+		print(f"Player: {utils.PLAYER}")
+		print(f"AI:     {utils.AI}")
+
+
+	def placeAStone(self, isAI, row, column):
+		self.boardMap[row][column] = 2 if isAI else 1
--- test.py
+++ test.py
@@ -3,6 +3,8 @@ from gomoku import Gomoku
 
 def main():
 	test_board = Gomoku()
+	test_board.placeAStone(True, 8, 3)
+	test_board.placeAStone(False, 7, 2)
 	test_board.drawBoardInTerminal()
 
 
