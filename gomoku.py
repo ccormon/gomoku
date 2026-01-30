9e5469a041a022e21654e0b57be86c48d0f27669
ccormon <camille.cormon@gmail.com>
2026-01-30 11:38:17 +0100

    create pattern dict and begin evaluate function

--- gomoku.py
+++ gomoku.py
@@ -16,16 +16,18 @@ class Gomoku():
 			self.boardMap[row][col] = 1 if isPlayer1 else 2
 
 
-	def isPositionOK(self, row, col):
+	def isInBoard(self, row, col):
 		if row < 0 or row > 18 or col < 0 or row > 18:
-			return False
-		if self.boardMap[row][col] != 0:
 			return False
 		return True
 
 
-	def evaluate(self, row, col):
-		alignments = self.getAllDirectionsAlignments(row, col)
+	def isPositionOK(self, row, col):
+		if not self.isInBoard(row, col):
+			return False
+		if self.boardMap[row][col] != 0:
+			return False
+		return True
 
 
 	def getAllDirectionsAlignments(self, row, col):
@@ -34,33 +36,28 @@ class Gomoku():
 
 		for direction in directions:
 			alignment = []
+			alignment.append(self.boardMap[row][col])
 			for i in range(5):
 				tested_row = row + i * direction[0]
 				tested_col = col + i * direction[1]
-				if self.isPositionOK(tested_row, tested_col):
+				if (not (tested_row == row and tested_col == col)
+					and self.isInBoard(tested_row, tested_col)):
 					alignment.append(self.boardMap[tested_row][tested_col])
 			for i in range(5):
 				tested_row = row - i * direction[0]
 				tested_col = col - i * direction[1]
-				if self.isPositionOK(tested_row, tested_col):
-					alignment.append(self.boardMap[tested_row][tested_col])
+				if (not (tested_row == row and tested_col == col)
+					and self.isInBoard(tested_row, tested_col)):
+					alignment.insert(0, self.boardMap[tested_row][tested_col])
 			alignments.append(alignment)
 
 		return alignments
 
 
-	def isWinningConfiguration(self, alignment, isPlayer1):
-		alignmentLen = alignment.size
-		winConfLen = utils.PLAYER1_WINCONF[0].size
-
-		if alignmentLen < winConfLen:
-			return False
-
-		winConf = utils.PLAYER1_WINCONF if isPlayer1 else utils.PLAYER2_WINCONF
-
-		for conf in winConf:
-			for i in range(alignmentLen - winConfLen + 1):
-				if alignment[i:i + winConfLen] == conf:
-					return True
-
-		return False
+	def evaluate(self, isPlayer1, row, col):
+		if not self.isPositionOK(row, col):
+			return 
+		self.boardMap[row][col] = 1 if isPlayer1 else 2
+		alignments = self.getAllDirectionsAlignments(row, col)
+		print(alignments)
+		self.boardMap[row][col] = 0
--- utils.py
+++ utils.py
@@ -5,17 +5,31 @@ PLAYER1 = 'X'
 PLAYER2 = 'O'
 EMPTY = '·'
 
-PLAYER1_WINCONF = [
-	[1, 1, 1, 1, 0],
-	[1, 1, 1, 0, 1],
-	[1, 1, 0, 1, 1],
-	[1, 0, 1, 1, 1],
-	[0, 1, 1, 1, 1]
-]
-PLAYER2_WINCONF = [
-	[2, 2, 2, 2, 0],
-	[2, 2, 2, 0, 2],
-	[2, 2, 0, 2, 2],
-	[2, 0, 2, 2, 2],
-	[0, 2, 2, 2, 2]
-]
+def create_pattern_dict():
+	EMPTY = 0
+	pattern_dict = {}
+
+	for player in (1, 2):
+		opponent = 2 if player == 1 else 1
+		sign = 1 if player == 1 else -1
+
+		pattern_dict[(player,)*5] = 1_000_000 * sign
+
+		pattern_dict[(EMPTY, player, player, player, player, EMPTY)] = 100_000 * sign
+		pattern_dict[(EMPTY, player, player, player, EMPTY, player, EMPTY)] = 100_000 * sign
+		pattern_dict[(EMPTY, player, EMPTY, player, player, player, EMPTY)] = 100_000 * sign
+		pattern_dict[(EMPTY, player, player, EMPTY, player, player, EMPTY)] = 100_000 * sign
+
+		pattern_dict[(opponent, player, player, player, player, opponent)] = -10 * sign
+
+		pattern_dict[(EMPTY, player, player, player, EMPTY)] = 1_000 * sign
+		pattern_dict[(EMPTY, player, EMPTY, player, player, EMPTY)] = 1_000 * sign
+		pattern_dict[(EMPTY, player, player, EMPTY, player, EMPTY)] = 1_000 * sign
+
+		pattern_dict[(opponent, player, player, player, opponent)] = -10 * sign
+
+		pattern_dict[(EMPTY, EMPTY, player, player, EMPTY)] = 100 * sign
+		pattern_dict[(EMPTY, player, player, EMPTY, EMPTY)] = 100 * sign
+		pattern_dict[(EMPTY, player, EMPTY, player, EMPTY)] = 100 * sign
+
+	return pattern_dict
