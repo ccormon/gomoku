7d39ef8e60614368689a0bcb9118497531671341
ccormon <camille.cormon@gmail.com>
2026-04-03 14:27:53 +0200

    add patterns

--- src/AI/utils.py
+++ src/AI/utils.py
@@ -1,73 +1,75 @@
-DEPTH = 5
+from dataclasses import dataclass
 
 
-PLAYER1 = 'X'
-PLAYER2 = 'O'
-EMPTY = '·'
+@dataclass(frozen=True)
+class Pattern:
+	pattern: tuple[int, ...]
+	score: int
 
-WIN_SCORE = 1_000_000
-
-
-def create_pattern_dict(isPlayer1: bool) -> dict:
-	EMPTY = "0"
-	pattern_dict = {}
-
-	for player in ("1", "2"):
-		opponent = "2" if player == "1" else "1"
-
-		if isPlayer1:
-			sign = 1 if player == "1" else -1
-		else:
-			sign = -1 if player == "1" else 1
-
-		def add(pattern, score):
-			pattern_dict["".join(pattern)] = score * sign
-
-		add((player, player, player, player, player), 1_000_000)
-		add((EMPTY, player, player, player, player, EMPTY), 100_000)
-		add((EMPTY, player, player, player, EMPTY, player, EMPTY), 10_000)
-		add((EMPTY, player, EMPTY, player, player, player, EMPTY), 10_000)
-		add((EMPTY, player, player, EMPTY, player, player, EMPTY), 10_000)
-		add((opponent, player, player, player, player, opponent), -10)
-		add((EMPTY, player, player, player, EMPTY), 1_000)
-		add((EMPTY, player, EMPTY, player, player, EMPTY), 1_000)
-		add((EMPTY, player, player, EMPTY, player, EMPTY), 1_000)
-		add((opponent, player, player, player, opponent), -10)
-		add((EMPTY, EMPTY, player, player, EMPTY), 100)
-		add((EMPTY, player, player, EMPTY, EMPTY), 100)
-		add((EMPTY, player, EMPTY, player, EMPTY), 100)
-
-	return pattern_dict
+	@property
+	def length(self):
+		return len(self.pattern)
 
 
-# def create_pattern_dict(isPlayer1: bool) -> dict:
-# 	EMPTY = 0
-# 	pattern_dict = {}
+patterns_player1 = [
+	Pattern((1, 1, 1, 1, 1), 2_000_000),
+	Pattern((0, 1, 1, 1, 1, 0), 100_000),
+	Pattern((1, 0, 1, 1, 1, 0), 50_000),
+	Pattern((0, 1, 1, 1, 0, 1), 50_000),
+	Pattern((0, 1, 1, 0, 1, 1, 0), 40_000),
+	Pattern((2, 1, 1, 1, 0, 1), 10_000),
+	Pattern((1, 0, 1, 1, 1, 2), 10_000),
+	Pattern((2, 1, 1, 1, 1, 0), 10_000),
+	Pattern((0, 1, 1, 1, 1, 2), 10_000),
+	Pattern((0, 1, 1, 1, 0), 5_000),
+	Pattern((0, 1, 0, 1, 1, 0), 3_000),
+	Pattern((0, 1, 1, 0, 1, 0), 3_000),
+	Pattern((2, 1, 1, 1, 0), 1_000),
+	Pattern((0, 1, 1, 1, 2), 1_000),
+	Pattern((2, 1, 1, 0, 1, 0), 500),
+	Pattern((0, 1, 0, 1, 1, 2), 500),
+	Pattern((0, 1, 1, 0), 300),
+	Pattern((0, 1, 0, 1, 0), 300),
+	Pattern((2, 1, 1, 0), 80),
+	Pattern((0, 1, 1, 2), 80),
+	Pattern((2, 1, 0, 1, 0), 40),
+	Pattern((0, 1, 0, 1, 2), 40),
+	Pattern((0, 1, 0), 20),
+	Pattern((2, 1, 0), 10),
+	Pattern((0, 1, 2), 10),
+	Pattern((2, 1, 1, 2), 10),
+	Pattern((2, 1, 1, 1, 2), 50),
+	Pattern((2, 1, 1, 1, 1, 2), 100),
+]
 
-# 	for player in (1, 2):
-# 		opponent = 2 if player == 1 else 1
-# 		if isPlayer1:
-# 			sign = 1 if player == 1 else -1
-# 		else:
-# 			sign = -1 if player == 1 else 1
 
-# 		pattern_dict[(player, player, player, player, player)] = 1_000_000 * sign
-
-# 		pattern_dict[(EMPTY, player, player, player, player, EMPTY)] = 100_000 * sign
-# 		pattern_dict[(EMPTY, player, player, player, EMPTY, player, EMPTY)] = 10_000 * sign
-# 		pattern_dict[(EMPTY, player, EMPTY, player, player, player, EMPTY)] = 10_000 * sign
-# 		pattern_dict[(EMPTY, player, player, EMPTY, player, player, EMPTY)] = 10_000 * sign
-
-# 		pattern_dict[(opponent, player, player, player, player, opponent)] = -10 * sign
-
-# 		pattern_dict[(EMPTY, player, player, player, EMPTY)] = 1_000 * sign
-# 		pattern_dict[(EMPTY, player, EMPTY, player, player, EMPTY)] = 1_000 * sign
-# 		pattern_dict[(EMPTY, player, player, EMPTY, player, EMPTY)] = 1_000 * sign
-
-# 		pattern_dict[(opponent, player, player, player, opponent)] = -10 * sign
-
-# 		pattern_dict[(EMPTY, EMPTY, player, player, EMPTY)] = 100 * sign
-# 		pattern_dict[(EMPTY, player, player, EMPTY, EMPTY)] = 100 * sign
-# 		pattern_dict[(EMPTY, player, EMPTY, player, EMPTY)] = 100 * sign
-
-# 	return pattern_dict
+patterns_player2 = [
+	Pattern((2, 2, 2, 2, 2), -2_000_000),
+	Pattern((0, 2, 2, 2, 2, 0), -100_000),
+	Pattern((2, 0, 2, 2, 2, 0), -50_000),
+	Pattern((0, 2, 2, 2, 0, 2), -50_000),
+	Pattern((0, 2, 2, 0, 2, 2, 0), -40_000),
+	Pattern((1, 2, 2, 2, 0, 2), -10_000),
+	Pattern((2, 0, 2, 2, 2, 1), -10_000),
+	Pattern((1, 2, 2, 2, 2, 0), -10_000),
+	Pattern((0, 2, 2, 2, 2, 1), -10_000),
+	Pattern((0, 2, 2, 2, 0), -5_000),
+	Pattern((0, 2, 0, 2, 2, 0), -3_000),
+	Pattern((0, 2, 2, 0, 2, 0), -3_000),
+	Pattern((1, 2, 2, 2, 0), -1_000),
+	Pattern((0, 2, 2, 2, 1), -1_000),
+	Pattern((1, 2, 2, 0, 2, 0), -500),
+	Pattern((0, 2, 0, 2, 2, 1), -500),
+	Pattern((0, 2, 2, 0), -300),
+	Pattern((0, 2, 0, 2, 0), -300),
+	Pattern((1, 2, 2, 0), -80),
+	Pattern((0, 2, 2, 1), -80),
+	Pattern((1, 2, 0, 2, 0), -40),
+	Pattern((0, 2, 0, 2, 1), -40),
+	Pattern((0, 2, 0), -20),
+	Pattern((1, 2, 0), -10),
+	Pattern((0, 2, 1), -10),
+	Pattern((1, 2, 2, 1), -10),
+	Pattern((1, 2, 2, 2, 1), -50),
+	Pattern((1, 2, 2, 2, 2, 1), -100),
+]
